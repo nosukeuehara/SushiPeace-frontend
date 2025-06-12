900a26382174f38594e19a90defa0c5d2f72b7b0
nosukeuehara <ryuunosuke06160616@gmail.com>
2025-06-13 00:35:09 +0900

    update : CSSのテンプレート定義

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,11 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+// このファイルは全てのページのラッパーコンポーネントを定義。
+// トップレベルのルートになる
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>{" "}
-        <Link to="/new" className="[&.active]:font-bold">
-          New
-        </Link>
-      </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
