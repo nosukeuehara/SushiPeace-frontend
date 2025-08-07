7ba87a498bd1f65f56962c9eb03a6ae61bf99653
nosukeuehara <ryuunosuke06160616@gmail.com>
2025-08-07 20:19:56 +0900

    update : 設定更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import {tanstackRouter} from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
       verboseFileRoutes: false,
     }),
     react(),
   ],
+  build: {
+    outDir: "dist",
+  },
   server: {
     host: true, // もしくは "0.0.0.0"
     port: 5173, // 必要に応じて変更
   },
-})
+});
