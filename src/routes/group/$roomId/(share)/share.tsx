63420d90a7239c7f699d5d7aac920ac0cf0b0efa
nosukeuehara <ryuunosuke06160616@gmail.com>
2025-08-08 22:50:58 +0900

    update : リンク生成

--- src/routes/group/$roomId/(share)/share.tsx
+++ src/routes/group/$roomId/(share)/share.tsx
@@ -5,11 +5,11 @@ export const Route = createFileRoute({
 });
 
 const SHARE_SUFFIX = "/share";
+const basePath = import.meta.env.VITE_BASE_PATH;
 
 function RouteComponent() {
   const {pathname} = useLocation();
   const {roomId} = useParams({strict: false});
-  const basePath = import.meta.env.VITE_BASE_PATH;
 
   const path = pathname.endsWith(SHARE_SUFFIX)
     ? pathname.slice(0, -SHARE_SUFFIX.length)
