924b298d50b2df5e40791c9ee6d3789e0c889a27
nosukeuehara <ryuunosuke06160616@gmail.com>
2025-10-05 02:36:15 +0900

    delete : 不要な関数削除

--- src/util/initCounts.ts
+++
@@ -1,8 +0,0 @@
-import type { PlateTemplate } from "../types/plate";
-
-export const createInitialCounts = (template: PlateTemplate) => {
-  return Object.keys(template.prices).reduce((acc, color) => {
-    acc[color] = 0;
-    return acc;
-  }, {} as Record<string, number>);
-};
